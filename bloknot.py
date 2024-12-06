9995eb5276c1a13db058b64b5980387de5070e3c
Ilya Osipov <io11223344111@gmail.com>
2024-12-06 23:14:57 +0300

    fon

--- bloknot.py
+++ bloknot.py
@@ -4,4 +4,10 @@ root=Tk()
 root.title('Текстовый редактор')
 root.geometry('600x700')
 
+f_text = Frame(root)
+f_text.pack(fill=BOTH, expand=1)
+
+text_fild=Text(f_text, bg='white',fg='black')
+text_fild.pack(expand=1, fill=BOTH, side=LEFT)
+
 root.mainloop()
