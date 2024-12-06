b00ec14a420d601bcdc35303c65d31014bd06ae9
Ilya Osipov <io11223344111@gmail.com>
2024-12-06 23:25:59 +0300

    oformlenie

--- bloknot.py
+++ bloknot.py
@@ -7,7 +7,11 @@ root.geometry('600x700')
 f_text = Frame(root)
 f_text.pack(fill=BOTH, expand=1)
 
-text_fild=Text(f_text, bg='white',fg='black')
+text_fild=Text(f_text, bg='white',fg='black', padx=10,pady=10, wrap=WORD,insertbackground='black', selectbackground='blue', spacing3=10,width=30)
 text_fild.pack(expand=1, fill=BOTH, side=LEFT)
+
+scroll=Scrollbar(f_text, command=text_fild.yview)
+scroll.pack(side=LEFT, fill=Y)
+text_fild.config(yscrollcommand=scroll.set)
 
 root.mainloop()
